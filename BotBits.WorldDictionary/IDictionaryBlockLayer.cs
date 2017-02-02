08be0add2213f0b71ced0ab67b5f5534cb029c55
Sepehr Farshid / Processor <sepehr1376@gmail.com>
2017-02-02 19:55:34 +0100

    Add GetByBlock

--- BotBits.WorldDictionary/IDictionaryBlockLayer.cs
+++ BotBits.WorldDictionary/IDictionaryBlockLayer.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace BotBits.WorldDictionary
 {
-    public interface IDictionaryBlockLayer<TId, TBlock, TItem>
+    public interface IDictionaryBlockLayer<TId, TBlock, out TItem>
         : IEnumerable<IBlockQuery<TId, TItem>>
         where TId : struct
         where TBlock : struct
@@ -11,6 +11,8 @@ namespace BotBits.WorldDictionary
         int Count { get; }
         IBlockQuery<TBlock, TItem> this[TBlock block] { get; }
         IBlockQuery<TId, TItem> this[TId id] { get; }
+
+        IBlockNestedQuery<TId, TBlock, TItem> GetByBlock(TId id);
 
         IEnumerable<IBlockQuery<TId, TItem>> GroupedById { get; }
         IEnumerable<IBlockQuery<TBlock, TItem>> GroupedByBlock { get; }
